462d8c18244f744e6614ad93322caf3621ef59b1
aureopaquete <aureopaquete007@outlook.com>
2018-06-15 12:37:37 +0100

    Commit 7

--- app/src/main/java/pt/ipg/calculadora/DbData.java
+++ app/src/main/java/pt/ipg/calculadora/DbData.java
@@ -1,15 +1,34 @@
 package pt.ipg.calculadora;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
 
 /**
  * Created by Aureo Paquete on 15/06/2018.
  */
+public class DbData implements BaseColumns {
+    public static final String TABLE_NAME = "Data";
 
-public class DbData {
+    public static final String[] ALL_COLUMNS = new String[]{};
+
+    private SQLiteDatabase db;
+    private String DbData_ID;
+
+
     public DbData(SQLiteDatabase db) {
     }
 
     public void create() {
+        db.execSQL(
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+
+                        DbData.TABLE_NAME +
+                        "(" + DbData_ID + ")" +
+                        ")"
+        );
     }
 }
+
+
+
--- app/src/main/java/pt/ipg/calculadora/DbResultados.java
+++ app/src/main/java/pt/ipg/calculadora/DbResultados.java
@@ -1,15 +1,34 @@
 package pt.ipg.calculadora;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
 
 /**
  * Created by Aureo Paquete on 15/06/2018.
  */
+public class DbResultados implements BaseColumns {
+    public static final String TABLE_NAME = "Resultados";
 
-public class DbResultados {
+    public static final String[] ALL_COLUMNS = new String[]{};
+
+    private SQLiteDatabase db;
+    private String DbResultados_ID;
+
+
     public DbResultados(SQLiteDatabase db) {
     }
 
     public void create() {
+        db.execSQL(
+                "CREATE TABLE " + TABLE_NAME + " (" +
+                        _ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+
+                        DbResultados.TABLE_NAME +
+                        "(" + DbResultados_ID + ")" +
+                        ")"
+        );
     }
 }
+
+
+
